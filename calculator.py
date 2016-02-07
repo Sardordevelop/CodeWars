476de7330b5d5a89c2cea6f344ab0efb296d4ab6
dshulgin2015 <unetby2016@yandex.ru>
2016-02-07 11:52:34 +0300

    Done

--- calculator.py
+++ calculator.py
@@ -1,11 +1,7 @@
-import pdb
+class Calculator(object):
+    """docstring for ClassName"""
 
-#tested:
-#2 * 4 * ( 5 + 2 / ( 10 * 2 ) )
-#( 44 + 4 ) * 25 / 5
-
-
-class Stack:
+    class Stack:
      def __init__(self):
          self.items = []
 
@@ -26,118 +22,135 @@ class Stack:
 
 
 
-operators = {
-    # addition
-    '+' : {
-        'prec' : 1,
-        'calc' : lambda a,b: a + b },
-    # subtraction
-    '-' : {
-        'prec' : 1,
-        'calc' : lambda a,b: a - b },
-    # multiplication
-    '*' : {
-        'prec' : 2,
-        'calc' : lambda a,b: a * b },
-    # division
-    '/' : {
-        'prec' : 2,
-        'calc' : lambda a,b: a / b },
-    # exponentiation
-    '^' : {
-        'prec' : 3,
-        'assoc' : 'right',
-        'calc' : lambda a,b: a ** b },
-    # negation
-    '~' : {
-        'prec' : 6,
-        'assoc' : 'right',
-        'arity' : 1,
-        'calc' : lambda a: -a },
-    # modulus
-    '%' : {
-        'prec' : 4,
-        'calc' : lambda a,b: a % b },
-    # factorial
-    '!' : {
-        'prec' : 4,
-        'assoc' : 'right',
-        'arity' : 1,
-        'calc' : lambda a: factorial(a) },
-    # average
-    '@' : {
-        'prec' : 5,
-        'calc' : lambda a,b: (a + b) / 2 },
-    # max
-    '$' : {
-        'prec' : 5,
-        'calc' : lambda a,b: max(a,b) },
-    # min
-    '&' : {
-        'prec' : 5,
-        'calc' : lambda a,b: min(a,b) } }
+    operators = {
+        # addition
+        '+' : {
+            'prec' : 1,
+            'calc' : lambda a,b: a + b },
+        # subtraction
+        '-' : {
+            'prec' : 1,
+            'calc' : lambda a,b: a - b },
+        # multiplication
+        '*' : {
+            'prec' : 2,
+            'calc' : lambda a,b: a * b },
+        # division
+        '/' : {
+            'prec' : 2,
+            'calc' : lambda a,b: a / b },
+        # exponentiation
+        '^' : {
+            'prec' : 3,
+            'assoc' : 'right',
+            'calc' : lambda a,b: a ** b },
+        # negation
+        '~' : {
+            'prec' : 6,
+            'assoc' : 'right',
+            'arity' : 1,
+            'calc' : lambda a: -a },
+        # modulus
+        '%' : {
+            'prec' : 4,
+            'calc' : lambda a,b: a % b },
+        # factorial
+        '!' : {
+            'prec' : 4,
+            'assoc' : 'right',
+            'arity' : 1,
+            'calc' : lambda a: factorial(a) },
+        # average
+        '@' : {
+            'prec' : 5,
+            'calc' : lambda a,b: (a + b) / 2 },
+        # max
+        '$' : {
+            'prec' : 5,
+            'calc' : lambda a,b: max(a,b) },
+        # min
+        '&' : {
+            'prec' : 5,
+            'calc' : lambda a,b: min(a,b) } }
+
+    def isfloat(self,x):
+        try:
+            float(x)
+            return True 
+        except ValueError:
+            return False
+
+    def toPolishNotation(self,infixNotation_):
+        stack = self.Stack()
+        polish_string = ""
+        infixNotation = infixNotation_.split()
+
+               
+        for x in infixNotation:
+                    
+            if self.isfloat(x):
+                polish_string += ' ' + x
+                #print(polish_string + " --")
+
+            if x in "+-*/":
+
+                if not stack.isEmpty():
+                    #print("here")
+                    while (not stack.isEmpty()) and (stack.peek() in "+-*/")  and self.operators[x]['prec'] <= self.operators[stack.peek()]['prec']:
+                        polish_string += ' ' +  stack.pop()
+                    stack.push(x)            
+                    #print(polish_string)
+                    #print(stack.peek() + "--stack peek")
+                else:
+                 stack.push(x)
+                 #print(polish_string + "--operator string")
+                 #print(stack.peek() + "--stack peek")
+                # print(stack.peek())
+
+            if x == '(':
+                stack.push(x)
 
 
-def toPolishNotation(infixNotation_):
-    stack = Stack()
-    polish_string = ""
-    infixNotation = infixNotation_.split()
-    
-    for x in infixNotation:
+            if x == ')':
+                if not stack.isEmpty():
 
-        if x.isdigit():
-            polish_string += ' ' + x
+                    while stack.peek() != '(':
+                        polish_string += ' ' + stack.pop()
+                    stack.pop()
 
-        if x in "+-*/":
-
-            if not stack.isEmpty:
-
-                while operators[x]['prec'] <= operators[stack.peek()]['prec'] or stack.isEmpty():
-                    polish_string += ' ' +  stack.pop()
-                stack.push(x)             
-
-            else: stack.push(x)
-
-        if x == '(':
-            stack.push(x)
-
-        if x == ')':
-            if not stack.isEmpty():
-
-                while stack.peek() != '(':
-                    polish_string += ' ' + stack.pop()
-                stack.pop()
-
-                if (not stack.isEmpty()) and (stack.peek() in "+-*/") :
-                    polish_string += ' ' + stack.pop()
-
-      #  print(polish_string)
+                    if (not stack.isEmpty()) and (stack.peek() in "+-*/") :
+                        polish_string += ' ' + stack.pop()
 
 
-    if not stack.isEmpty():
-        while not stack.isEmpty():
-            polish_string += ' ' +  stack.pop()
+        if not stack.isEmpty():
+            while not stack.isEmpty():
+                polish_string += ' ' +  stack.pop()
 
-    return polish_string
-
-
-
-def calculate(sRPN_):
-    sRPN = sRPN_.split()
-    stack = Stack()
-    for x in sRPN:
-        if x.isdigit():
-            #print("operand")
-            stack.push(x)
-            #print(stack.peek())
-        else:
-            #print("operator")
-            b = float(stack.pop())
-            a = float(stack.pop())
-            stack.push(operators[x]['calc'](a, b))
-            #print(stack.peek())
-    return stack.pop()
+        return polish_string
 
 
 
-print(calculate(toPolishNotation("2 * 4 * ( 5 + 2 / ( 10 * 2 ) ) ")))
+    def calculate(self,sRPN_):
+
+        sRPN = sRPN_.split()
+        stack = self.Stack()
+        for x in sRPN:
+            if self.isfloat(x):
+                #print("operand")
+                stack.push(x)
+                #print(stack.peek())
+            else:
+                #print("operator")
+                b = float(stack.pop())
+                a = float(stack.pop())
+                stack.push(self.operators[x]['calc'](a, b))
+                #print(stack.peek())
+        return stack.pop()
+
+    def evaluate(self,string):
+        rpn = self.toPolishNotation(string)
+        print(rpn)
+        return round(float(self.calculate(rpn)),3)
+
+
+print(Calculator().evaluate("1.1 * 2.2 * 3.3"))
